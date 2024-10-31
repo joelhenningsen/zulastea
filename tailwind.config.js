e971d31357ded4eb076b393bb23a0183608f2bbe
joelhenningsen <ravajoely@gmail.com>
2024-10-30 17:56:35 -0700

    Move index to root and update pathing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/index.html",        // Path to your main HTML file in the `src` folder
+    "./index.html",        // Path to your main HTML file in the `src` folder
     "./src/**/*.{html,js}",    // All HTML and JS files in the `src` folder
   ],
   theme: {
