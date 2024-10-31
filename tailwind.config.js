f3e2681a856238647e437508bfc9f026fa120ba8
joelhenningsen <ravajoely@gmail.com>
2024-10-30 17:31:08 -0700

    Updated to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/index.html",        // Path to your main HTML file in the `src` folder
+    "./src/**/*.{html,js}",    // All HTML and JS files in the `src` folder
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'zulasLightPink': '#FFE7F3',
+        'zulasDarkerPink': '#EFA0CB',
+      },
+    }
   },
   plugins: [],
 }
